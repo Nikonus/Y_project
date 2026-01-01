416b3bdd40544f532688e49535eb329a6d8cd42f
Nikonus <nikhildubay.2005@gmail.com>
2026-01-01 20:35:38 +0530

    Mount user router in Express app

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
-import expresss from "express";
+import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-
-
-const app = expresss();
+const app = express();
 
 app.use(cors({
-    origin:process.env.CORS.ORIGIN,
-    Credentials:true
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
 }));
-app.use(expresss.json({limit:"16kb"}));
-app.use(expresss.urlencoded({extended:true , limit:"16kb"}));
-app.use(expresss.static("public"));
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 app.use(cookieParser());
 
-
-
+// mount router
+import router from "./routes/user.routes.js";
+app.use("/api/v1/users", router);
 
 export default app;
