f28227b55444356b67e9c72eb57a21594c5a53c6
Nikonus <nikhildubay.2005@gmail.com>
2026-01-02 11:18:11 +0530

    feat: wire user registration route to controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
 import registerUser from "../controlers/user.controler.js";
+import { upload } from "../middilewares/multer.middileware.js";
 
 const router = Router();
 
-router.post("/register", registerUser);
+router.route("/register").post(
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:1
+        }
+    ]),
+    registerUser
+)
 
 export default router;
