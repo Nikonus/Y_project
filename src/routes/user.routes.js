a23503b370c06aecee50fd7000bddef57a7427ea
Nikonus <nikhildubay.2005@gmail.com>
2026-01-02 20:51:14 +0530

    feat(routes): update user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,18 +4,13 @@ import { upload } from "../middilewares/multer.middileware.js";
 
 const router = Router();
 
-router.route("/register").post(
-    upload.fields([
-        {
-            name:"avatar",
-            maxCount:1
-        },
-        {
-            name:"coverImage",
-            maxCount:1
-        }
-    ]),
-    registerUser
-)
+router.post(
+  "/register",
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 }
+  ]),
+  registerUser
+);
 
 export default router;
