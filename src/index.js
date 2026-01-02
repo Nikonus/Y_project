9e33320b8df5969f02798358f072f4b7932bd049
Nikonus <nikhildubay.2005@gmail.com>
2026-01-02 11:17:54 +0530

    chore: stabilize server startup and environment loading

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import "dotenv/config";
 import express from "express";
 import connectDB from "./db/index.js";
 import app from "./app.js";
+import process from "process";
+
+process.setMaxListeners(0);
+
 
 
 
