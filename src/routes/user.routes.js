17b55cadf4b4907cffc03b8ab4d8f37b9f2d488a
Nikonus <nikhildubay.2005@gmail.com>
2026-01-03 12:19:16 +0530

    feat(routes): add protected logout route with JWT middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import registerUser from "../controlers/user.controler.js";
 import { upload } from "../middilewares/multer.middileware.js";
+import { loginUser,logoutUser } from "../controlers/user.controler.js";
+import { veryfyJWT } from "../middilewares/Auth.middileware.js";
+
 
 const router = Router();
 
@@ -12,5 +15,8 @@ router.post(
   ]),
   registerUser
 );
+router.post("/login", loginUser);
+router.post("/logout",veryfyJWT ,logoutUser);
+
 
 export default router;
