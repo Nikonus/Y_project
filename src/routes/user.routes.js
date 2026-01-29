86091c0c7d81e0b95f603204d350960736fb7971
Nikonus <nikhildubay.2005@gmail.com>
2026-01-29 20:11:57 +0530

    added user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import {registerUser }from "../controlers/user.controler.js";
-import { upload } from "../middilewares/multer.middileware.js";
-import { loginUser,logoutUser } from "../controlers/user.controler.js";
-import { veryfyJWT } from "../middilewares/Auth.middileware.js";
-import { refreshAccessToken } from "../controlers/user.controler.js";
+import {
+  getUserChannalProfile,
+  registerUser,
+  updateUserAvatar,
+  updatrAccountDetails,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changecurrentuserpassword,
+  getCurrentuser,
+  updateUserCoverImage,
+  userWatchHistory
+} from "../controlers/user.controler.js";
 
+import { upload } from "../middilewares/multer.middileware.js";
+import { veryfyJWT } from "../middilewares/Auth.middileware.js";
 
 const router = Router();
 
@@ -16,9 +26,19 @@ router.post(
   ]),
   registerUser
 );
+
 router.post("/login", loginUser);
-router.post("/logout",veryfyJWT ,logoutUser);
+router.post("/logout", veryfyJWT, logoutUser);
 router.post("/refresh-token", refreshAccessToken);
 
+router.post("/change-password", veryfyJWT, changecurrentuserpassword);
+router.post("/current-user", veryfyJWT, getCurrentuser);
+
+router.patch("/update-profile", veryfyJWT, updatrAccountDetails);
+router.patch("/avatar", veryfyJWT, upload.single("avatar"), updateUserAvatar);
+router.patch("/cover-image", veryfyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.get("/c/:username", veryfyJWT, getUserChannalProfile);
+router.get("/watch-history", veryfyJWT, userWatchHistory);
 
 export default router;
