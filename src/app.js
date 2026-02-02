48b6fbec3a1439760f4c525bb656e64333d15962
Nikonus <nikhildubay.2005@gmail.com>
2026-02-02 22:08:43 +0530

    feat: update app configuration for comment routes integration

--- src/app.js
+++ src/app.js
@@ -17,7 +17,9 @@ app.use(cookieParser());
 // mount router
 import router from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comments.routes.js";
 app.use("/api/v1/users", router);
 app.use("/api/v1/videos", videoRouter); 
+app.use("/api/v1/comments", commentRouter);
 
 export default app;
