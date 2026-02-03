d2772408cc340d3619cecb665837834e23740119
Nikonus <nikhildubay.2005@gmail.com>
2026-02-03 12:59:54 +0530

    feat: integrate like and subscription routes into app

--- src/app.js
+++ src/app.js
@@ -18,8 +18,12 @@ app.use(cookieParser());
 import router from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comments.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 app.use("/api/v1/users", router);
 app.use("/api/v1/videos", videoRouter); 
 app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 export default app;
