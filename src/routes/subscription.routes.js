2c6eaf84760eb6d8b3e456d2024409afef5e3c8b
Nikonus <nikhildubay.2005@gmail.com>
2026-02-03 19:29:51 +0530

    feat: update subscription routes structure

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,7 +1,17 @@
-router.use(verifyJWT); // Pehle login verify karo
+import { Router } from "express";
+import {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+} from "../controlers/subscription.controler.js";
+import { veryfyJWT } from "../middilewares/Auth.middileware.js"; 
+const router = Router();
+router.use(veryfyJWT); 
 
 router.route("/c/:channel_id")
     .get(getUserChannelSubscribers)
     .post(toggleSubscription);
 
 router.route("/u/:subscriber_id").get(getSubscribedChannels);
+
+export default router;
