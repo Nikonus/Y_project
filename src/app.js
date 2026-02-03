9f93f769031c97487d05c0af97412440be0f92d8
Nikonus <nikhildubay.2005@gmail.com>
2026-02-03 19:30:00 +0530

    feat: integrate tweet routes into app configuration

--- src/app.js
+++ src/app.js
@@ -20,10 +20,12 @@ import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comments.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
 app.use("/api/v1/users", router);
 app.use("/api/v1/videos", videoRouter); 
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/tweets", tweetRouter);
 
 export default app;
