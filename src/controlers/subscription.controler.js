0a60f7e68e4dbfd158a8924bfa87671e6c6ada08
Nikonus <nikhildubay.2005@gmail.com>
2026-02-06 22:26:26 +0530

    feat: enhance subscription controller features

--- src/controlers/subscription.controler.js
+++ src/controlers/subscription.controler.js
@@ -11,6 +11,7 @@ import { Video } from "../models/video.model.js";
 import { Subscription } from "../models/subscription.model.js";
 
 
+
 const toggleSubscription = asyncHandler(async (req, res, next) => {
     const { channel_id } = req.params;
 
@@ -22,115 +23,93 @@ const toggleSubscription = asyncHandler(async (req, res, next) => {
         return next(new Apierr("You cannot subscribe to yourself", 400));
     }
 
-    // INDUSTRY FIX: Always query the dedicated Subscription model
-    const existingSubscription = await Subscription.findOne({
+    const channelExists = await User.exists({ _id: channel_id });
+    if (!channelExists) {
+        return next(new Apierr("Channel not found", 404));
+    }
+
+    const existingSubscription = await Subscription.findOneAndDelete({
         subscriber: req.user._id,
         channel: channel_id
     });
 
     if (existingSubscription) {
-        // Unsubscribe logic
-        await Subscription.findByIdAndDelete(existingSubscription._id);
-
         return res.status(200).json(
             new Apiresponse(200, { isSubscribed: false }, "Unsubscribed successfully")
         );
-    } else {
-        // Subscribe logic
-        const newSub = await Subscription.create({
-            subscriber: req.user._id,
-            channel: channel_id
-        });
-
-        return res.status(201).json(
-            new Apiresponse(201, { isSubscribed: true }, "Subscribed successfully")
-        );
     }
+
+    await Subscription.create({
+        subscriber: req.user._id,
+        channel: channel_id
+    });
+
+    return res.status(201).json(
+        new Apiresponse(201, { isSubscribed: true }, "Subscribed successfully")
+    );
 });
+
 
 
 const getUserChannelSubscribers = asyncHandler(async (req, res, next) => {
     const { channel_id } = req.params;
+
     if (!mongoose.isValidObjectId(channel_id)) {
         return next(new Apierr("Invalid channel ID", 400));
     }
-    const subscriber = await Subscription.aggregate([
-        {
-            $match: {
-                channel: new mongoose.Types.ObjectId(channel_id)
-            }
-        },
+
+    const subscribers = await Subscription.aggregate([
+        { $match: { channel: new mongoose.Types.ObjectId(channel_id) } },
         {
             $lookup: {
                 from: "users",
                 localField: "subscriber",
                 foreignField: "_id",
-                as: "subscriberDetails"
-                ,pipeline: [
-                    {
-                        $project: { 
-                            username: 1,
-                            fullname: 1,
-                            avatar: 1
-                        }
-                    }
-                ]
+                as: "subscriberDetails",
+                pipeline: [{ $project: { username: 1, fullname: 1, avatar: 1 } }]
             }
         },
-        {
-            $unwind: "$subscriberDetails"
-        },
-        {
-            $project: {
-                _id: 0,
-                subscriberDetails: 1
-            }
-        }
+        { $unwind: "$subscriberDetails" },
+        { $project: { _id: 0, subscriberDetails: 1 } }
     ]);
-    return res.status(200).json(new Apiresponse(200, subscriber, "Subscribers fetched successfully"));
+
+    return res.status(200).json(
+        new Apiresponse(200, { count: subscribers.length, subscribers }, "Subscribers fetched successfully")
+    );
 });
 
+
 const getSubscribedChannels = asyncHandler(async (req, res, next) => {
-   const {subscriber_id} = req.params;
-   if(!mongoose.isValidObjectId(subscriber_id)){
-    return next(new Apierr("Invalid subscriber ID", 400));
-   }  
-   const SubscribedChannels = await Subscription.aggregate([
-        {
-            $match: {
-                subscriber: new mongoose.Types.ObjectId(subscriber_id)
-            }
-        },
+    const { subscriber_id } = req.params;
+
+    if (!mongoose.isValidObjectId(subscriber_id)) {
+        return next(new Apierr("Invalid subscriber ID", 400));
+    }
+
+    // 🔒 Uncomment if subscriptions should be private
+    // if (req.user._id.toString() !== subscriber_id) {
+    //     return next(new Apierr("Unauthorized", 403));
+    // }
+
+    const channels = await Subscription.aggregate([
+        { $match: { subscriber: new mongoose.Types.ObjectId(subscriber_id) } },
         {
             $lookup: {
                 from: "users",
                 localField: "channel",
                 foreignField: "_id",
                 as: "channelDetails",
-                pipeline: [
-                    {
-                        $project: {
-                            username: 1,
-                            fullname: 1,
-                            avatar: 1,
-                            
-                        }
-                    }
-                ]
+                pipeline: [{ $project: { username: 1, fullname: 1, avatar: 1 } }]
             }
         },
-        {
-            $unwind: "$channelDetails"
-        },
-        {
-            $project: {
-                _id: 0,
-                channelDetails: 1
-            }
-        }
+        { $unwind: "$channelDetails" },
+        { $project: { _id: 0, channelDetails: 1 } }
     ]);
-    return res.status(200).json(new Apiresponse(200, SubscribedChannels, "Subscribed channels fetched successfully"));
-}); 
+
+    return res.status(200).json(
+        new Apiresponse(200, { count: channels.length, channels }, "Subscribed channels fetched successfully")
+    );
+});
 
 
 export {
